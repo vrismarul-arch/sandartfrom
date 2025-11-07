ac20aabe7c0d8cb61b6f1dd2af3ada0655696c27
vrismarul-arch <vrismarul@gmail.com>
2025-11-07 20:13:36 +0530

    main

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -93,7 +93,7 @@ export default function BookingForm({ onSuccess }) {
       });
 
       form.resetFields();
-
+// min
       // ✅ Notification JSX safe
       setNotification({
         status: "success",
